b12437e0ef489264a5b4201787b7c3141131191f
flouou <flo.bind@web.de>
2015-02-19 21:58:49 +0100

    Swipe
    Swipe sollte funktionieren, yeha.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,22 @@ $(document).ready(function(){
         $('.main').toggleClass('no-scroll');
         $('.shadow').fadeToggle("fast","linear");
     });
+    $('body').on('swiperight',function(){
+        $('ul').addClass('showMenu');
+        $('.main').addClass('slideBody');
+        $('.main').addClass('no-scroll');
+        $('.shadow').fadeIn("fast","linear");
+    });
     $('.shadow').on('click',function(){
         $('ul').toggleClass('showMenu');
         $('.main').toggleClass('slideBody');
         $('.main').toggleClass('no-scroll');
         $('.shadow').fadeToggle("fast","linear");
+    });
+    $('body').on('swipeleft',function(){
+        $('ul').removeClass('showMenu');
+        $('.main').removeClass('slideBody');
+        $('.main').removeClass('no-scroll');
+        $('.shadow').fadeOut("fast","linear");
     });
 });
