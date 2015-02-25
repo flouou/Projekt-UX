ecd012582d15443746c3d626d41b92ff84baf105
kiniimod <dhb.bernie@web.de>
2015-02-25 16:39:59 +0100

    Prototyping Map Swipe-Effekt

---
+++ ppp_1/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#mapButtonRight').on('click',function(){
+       $('#content').toggleClass('swipeToLeft');
+        //alert("Hello!");
+    });
+    /*$('body').on('swiperight',function(){
+        $('ul').addClass('showMenu');
+        $('.main').addClass('slideBody');
+        $('.main').addClass('no-scroll');
+        $('.shadow').fadeIn("fast","linear");
+    });
+    $('.shadow').on('click',function(){
+        $('ul').toggleClass('showMenu');
+        $('.main').toggleClass('slideBody');
+        $('.main').toggleClass('no-scroll');
+        $('.shadow').fadeToggle("fast","linear");
+    });*/
+    $('#body').on('swipeleft',function(){
+        $(body).toggleClass('swipeToLeft');
+    });
+});
