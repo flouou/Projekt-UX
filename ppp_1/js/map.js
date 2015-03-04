0eeba0c119424a90d9bd6e391c9f9ab3d271c711
kiniimod <dhb.bernie@web.de>
2015-03-04 19:39:48 +0100

    Map mit Custom-Icons
    Karte um eigene Icons ergänzt. Parkhaus-Icons zeigen Parkhausbelegung
    mit Ampel-Symbol.

--- ppp_1/js/map.js
+++ ppp_1/js/map.js
@@ -7,6 +7,9 @@ var lgEStationen = L.layerGroup();
 var greenPHIcon;
 var yellowPHIcon;
 var redPHIcon;
+var parkplatzIcon;
+var eStationIcon;
+//var kostenlosIcon;
 var iconWidth = 43;
 var iconHeight = 54;
 var iconAnchorX = 21.5;
@@ -19,15 +22,36 @@ $(document).ready(function () {
     $(".leaflet-control-zoom").css("visibility", "hidden");
 
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-        maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
             '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'examples.map-i875mjb7'
     }).addTo(map);
     
+    map.locate({setView: true, maxZoom: 16});
+    
     greenPHIcon = L.icon({
                     iconUrl: 'images/icons/parkhaus_icon_grün_klein.png',
+                    iconSize:[iconWidth,iconHeight],
+                    iconAnchor: [iconAnchorX,iconAnchorY],
+                    popupAnchor: [iconPopupAnchorX,iconPopupAnchorY]});
+    redPHIcon = L.icon({
+                    iconUrl: 'images/icons/parkhaus_icon_rot_klein.png',
+                    iconSize:[iconWidth,iconHeight],
+                    iconAnchor: [iconAnchorX,iconAnchorY],
+                    popupAnchor: [iconPopupAnchorX,iconPopupAnchorY]});
+    yellowPHIcon = L.icon({
+                    iconUrl: 'images/icons/parkhaus_icon_gelb_klein.png',
+                    iconSize:[iconWidth,iconHeight],
+                    iconAnchor: [iconAnchorX,iconAnchorY],
+                    popupAnchor: [iconPopupAnchorX,iconPopupAnchorY]});
+    parkplatzIcon = L.icon({
+                    iconUrl: 'images/icons/parkplatz_icon_klein.png',
+                    iconSize:[iconWidth,iconHeight],
+                    iconAnchor: [iconAnchorX,iconAnchorY],
+                    popupAnchor: [iconPopupAnchorX,iconPopupAnchorY]});
+    eStationIcon = L.icon({
+                    iconUrl: 'images/icons/estation_icon_klein.png',
                     iconSize:[iconWidth,iconHeight],
                     iconAnchor: [iconAnchorX,iconAnchorY],
                     popupAnchor: [iconPopupAnchorX,iconPopupAnchorY]});
@@ -57,25 +81,42 @@ function printParkmarker() {
     for (var j in result) {
         var x = result[j].LAT;
         var y = result[j].LONG;
-        var marker = L.marker([x, y],{icon: greenPHIcon});
+        var marker = L.marker([x, y]);
+        
         if (result[j].TYPE == 'Parkhaus') {
-            var t = "<strong>" + result[j].PH_NAME + "</strong>, " + result[j].PH_STRASSE + " (" + result[j].STADTTEIL + ")<br>" + result[j].FREIE_PARKPLAETZE + " von " + result[j].GESAMTE_PARKPLAETZE + " Plätzen frei.";
+            var ges = result[j].GESAMTE_PARKPLAETZE;
+            var frei = result[j].FREIE_PARKPLAETZE;
+            var bel = 1-(frei/ges);
+            t = "<strong>" + result[j].PH_NAME + "</strong>, " + result[j].PH_STRASSE + " (" + result[j].STADTTEIL + ")<br>" + frei + " von " + ges + " Plätzen frei.";
+            
+            if(bel<=0.5){
+                marker = L.marker([x,y], {icon: greenPHIcon});    
+            } else if(bel > 0.5 && bel < 0.85){
+                marker = L.marker([x,y], {icon: yellowPHIcon});
+            } else {
+                marker = L.marker([x,y], {icon: redPHIcon});
+            }
+            
             marker.bindPopup(t);
             lgParkhaus.addLayer(marker);
             map.addLayer(lgParkhaus);
         } else if (result[j].TYPE == 'Parkplatz') {
-            var t = "<strong>" + result[j].STANDORT + "</strong><br>" +
+            t = "<strong>" + result[j].STANDORT + "</strong><br>" +
                 result[j].STELLPLÄTZE + " Plätze verfügbar<br>" +
                 "Höchst-Park-Dauer: " + result[j].HÖCHST_PARK_DAUER + ",<br>" +
                 "Gebührenpflichtiger Zeitraum: " + result[j].GEBÜHRENPFL_PARKZEIT + ",<br>" +
                 "Gebühren: " + result[j].GEBÜHREN;
+            
+            marker = L.marker([x,y], {icon: parkplatzIcon});
             marker.bindPopup(t);
-           lgParkplaetze.addLayer(marker);
+            lgParkplaetze.addLayer(marker);
             map.addLayer(lgParkplaetze);
         } else if (result[j].TYPE == 'ELadestation') {
-            var t = "<strong>" + result[j].STANDORT + "</strong> (" + result[j].STADTTEIL + ")<br>" +
+            t = "<strong>" + result[j].STANDORT + "</strong> (" + result[j].STADTTEIL + ")<br>" +
                 "Steckertyp: " + result[j].STECKDOSE + ",<br>" +
                 "Ladestation ist " + result[j].LADESTATION + ".";
+            
+            marker = L.marker([x,y], {icon: eStationIcon});
             marker.bindPopup(t);
             lgEStationen.addLayer(marker);
             map.addLayer(lgEStationen);
