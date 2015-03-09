9a73de11494f4b33914c35e947a6d618728b8828
flouou <flo.bind@web.de>
2015-03-09 15:29:12 +0100

    Accordion Fix
    Accordion öffnet sich jetzt nicht mehr so seltsam doppelt, wenn man auf
    den Pfeil klickt.

--- js/accordion.js
+++ js/accordion.js
@@ -10,11 +10,10 @@ $(document).ready(function() {
         // Grab current anchor value
         var currentAttrValue = $(this).attr('href');
  
-        if($(e.target).is('.active')) {
+        if($(e.target).is('.active') || $(e.target).parent().is('.active')) {
             close_accordion_section();
         }else {
             close_accordion_section();
- 
             // Add active class to section title
             $(this).addClass('active');
             // Open up the hidden content panel
