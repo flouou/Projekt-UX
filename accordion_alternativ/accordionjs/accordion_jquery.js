f904e3c9516759cf732d7185a22ce6a0569e5331
flouou <flo.bind@web.de>
2015-03-11 12:50:59 +0100

    Alternatives Accordion produktiv
    Habe das alternative Accordion produktiv ins System aufgenommen.
    profil.html ist jetzt das neue, aktuelle Profil und profil_alt.html das
    alte.

---
+++ accordion_alternativ/accordionjs/accordion_jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#profilContent').accordion({
+        icons: { "header": "ion-chevron-down", "activeHeader": "ion-chevron-up" },
+        collapsible: true,
+        heightStyle: "content",
+        active: false
+    });
+});
--- js/accordion_jquery.js
+++ js/accordion_jquery.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#profilContent').accordion({
+        icons: { "header": "ion-chevron-down", "activeHeader": "ion-chevron-up" },
         collapsible: true,
         heightStyle: "content",
         active: false
